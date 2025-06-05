e64cb18af88dddea94f3630aa761b314f063e6f9
Michael Akimov <michael@island-future.ru>
2025-06-06 00:00:52 +0300

    simple html in php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+    <title>Занятие 1 - Приветствие.</title>
+</head>
+<body>
+    <h1>Первое занятие.</h1>
+    <form action="index.php" method="get">
+        <p>Как Тебя зовут?</p>
+        <input type="text" name="name" value="">
+        <input type="submit" value="Приятно познакомиться">
+    </form>
+</body>
+</html>
