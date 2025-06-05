2220ccf3d2869aefe4caaf86d0e871cb4272e4d1
Michael Akimov <michael@island-future.ru>
2025-06-06 00:15:25 +0300

    Код для первого урока - валидируем данные

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php
+// инициализируем переменную пустой строкой
 $yourName = '';
 if (! empty($_GET['name'])) {
-    $yourName = $_GET['name'];
+    // так как HTML-тегов в имени быть не может и ничего кроме букв, тире и пробела тоже - то удалим все лишнее
+
+    // записать это можно вот так ...
+    $yourName = trim($_GET['name']); // убираем пробелы с боков
+    $yourName = strip_tags($yourName); // удаляем все html-теги
+    $yourName = preg_replace('/[^a-zа-яЁё `-]/iug', '', $yourName); // удаляем все символы, кроме букв латинского и русского алфавита, а также пробела и символа тире и апострофа
+
+    // ... или так
+    //$yourName = preg_replace('/[^a-zа-яЁё `-]/iug', '', strip_tags(trim($_GET['name'])));
 }
 ?>
 <!DOCTYPE html>
 <html lang="ru">
-    
+
 <head>
-    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
+    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
     <title>Занятие 1 - Приветствие.</title>
 </head>
 
@@ -21,7 +30,7 @@ if (! empty($_GET['name'])) {
         <input type="submit" value="Приятно познакомиться">
     </form>
     <?php else: ?>
-    <p>Приветствую, <?=$yourName?></p>
+    <p>Приветствую, <?=htmlentities($yourName)?></p>
     <?php endif; ?>
 </body>
 
